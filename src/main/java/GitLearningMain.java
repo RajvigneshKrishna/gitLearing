8d975c0fecf40f1408f896f69256f1bf5f6aa89e
unknown <rajvigneshkrishna@gmail.com>
2022-09-27 12:17:06 +0530

    Addtion changes introduced

--- src/main/java/GitLearningMain.java
+++ src/main/java/GitLearningMain.java
@@ -1,7 +1,10 @@
 public class GitLearningMain {
 
 	public static void main(String[] args) {
+		int a = 10;
+		int b = 20
 		System.out.println("Hello World Rajvignesh");
+		System.out.println(a+b);
 	}
 
 }
